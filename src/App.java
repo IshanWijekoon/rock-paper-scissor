f46320fbe0bab43fca3a580c85a8a7ed9751136f
Ishan Wijekoon <ishancoc295@gmail.com>
2025-06-09 21:58:18 +0530

    added the base code

---
+++ src/App.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+public class App {
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				// instantiate a RockPaperScisorGUI obj
+				RockPaperScissorGUI rockPaperScissorGUI = new RockPaperScissorGUI();
+				
+				// display the GUI
+				rockPaperScissorGUI.setVisible(true);
+			}
+			
+		});
+		
+
+	}
+
+}
